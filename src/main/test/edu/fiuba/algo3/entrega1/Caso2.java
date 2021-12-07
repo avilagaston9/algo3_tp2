b8f6bb1099712eb03281c1e2c025241ca32f62e5
Gastón <gavila@fi.uba.ar>
2021-12-07 17:37:39 -0300

    Agrego test caso 2

--- src/main/test/edu/fiuba/algo3/entrega1/Caso2.java
+++ src/main/test/edu/fiuba/algo3/entrega1/Caso2.java
@@ -1,6 +1,4 @@
 package edu.fiuba.algo3.entrega1;
-
-
 import clases.Edificio;
 import clases.Pista;
 import clases.Ciudad
@@ -10,21 +8,30 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Caso1 {
+public class Caso2 {
 
     @Test
-    public void NovatoComienzaEnMontrealVisitaBancoDespliegaPistaCorrecta(){
+    public void NovatoComienzaEnMontrealVisitaBancoYDespliegaPistaCorrecta(){
         //set up
         Ciudad montreal = new Ciudad("Montreal");
         Ciudad buenosAires = new Ciudad("Buenos Aires");
 
         Pistas pistasBanco = new Pistas();
+        Pistas pistasBiblioteca = new Pistas();
+
         Pista pistaBuenosAires = new Pista("pesos");
+        Pista otraPistaBuenosAires = new Pista("Obelisco");
+
         pistasBanco.agregarPistaFacil(pistaBuenosAires);
+        pistasBiblioteca.agregarPistaFacil(otraPistaBuenosAires);
+
         buenosAires.agregarPistasBanco(pistasBanco);
+        buenosAires.agregarPistasBiblioteca(pistasBiblioteca);
 
         Edificio banco = new Banco();
+        Edificio biblioteca = new Biblioteca();
         montreal.agregarEdificio(banco);
+        montreal.agregarEdificio(biblioteca);
 
         Ladron maria = new Ladron("Maria");
         buenosAires.agregarLadron(maria);
@@ -35,8 +42,15 @@ public class Caso1 {
         int cantArrestos = 0;
         Policia policia = new Policia(cantArrestos, montreal); // crear tiempo y rango
 
-        List pistas = policia.visitar(banco);
+        //act
+        List pistasPrimeraVisita = policia.visitar(banco);
+        List pistasSegundaVisita = policia.visitar(biblioteca);
 
-        assertEquals(pistas.get(0).revelar(), pistaBuenosAires.revelar());
+        //assert
+        assertEquals(pistasPrimeraVisita.get(0).revelar(), pistaBuenosAires.revelar());
+        assertEquals(pistasSegundaVisita.get(0)).revelar(), otraPistaBuenosAires.revelar());
     }
 }
+
+
+
