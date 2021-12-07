5ad7a1aebfee523d47f1fcc9a7f2c0074026afc2
Gastón <gavila@fi.uba.ar>
2021-12-07 19:13:30 -0300

    Corrijo nombre prueba caso 2

---
+++ src/main/test/edu/fiuba/algo3/entrega1/Caso4.java
@@ -0,0 +1,12 @@
+package edu.fiuba.algo3.entrega1;
+
+import org.junit.jupiter.api.Test;
+
+public class Caso4 {
+
+    @Test
+    public void NovatoComienzaEnMontrealVisitaBancoDespliegaPistaCorrecta(){
+
+
+    }
+}
