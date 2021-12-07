2f4c0d417b57efe92c11c7b02a96e19a4180ac12
Gastón <gavila@fi.uba.ar>
2021-12-07 20:31:21 -0300

    Corrijo caso 4

--- src/main/test/edu/fiuba/algo3/entrega1/Caso4.java
+++ src/main/test/edu/fiuba/algo3/entrega1/Caso4.java
@@ -36,6 +36,39 @@ public class Caso4 {
 
             pistas = policia.visitar(aeropuerto);
             assertEquals(pistas.get(0).revelar(), pistaBuenosAires.revelar());
+        }
+        assertEquals(policia.tiempoTranscurrido(), 0);
+    }
+
+    @Test
+    public void NovatoVisitaUnAeropuertoTresVeces(){
+
+        //arrange
+        Ciudad montreal = new Ciudad("Montreal");
+        Ciudad buenosAires = new Ciudad("Buenos Aires");
+
+        Pistas pistasAeropuerto = new Pistas();
+        Pista pistaBuenosAires = new Pista("Su bandera es blanca y celeste");
+        pistasAeropuerto.agregarPistaFacil(pistaBuenosAires);
+        buenosAires.agregarPistasAeropuerto(pistasAeropuerto);
+
+        Edificio aeropuerto = new Aeropuerto();
+        montreal.agregarEdificio(aeropuerto);
+
+        Ladron maria = new Ladron("Maria");
+        buenosAires.agregarLadron(maria);
+        int distancia = 9000;
+        montreal.agregarSiguiente(buenosAires, distancia);
+
+        int cantArrestos = 0;
+        Policia policia = new Policia(cantArrestos, montreal); // crear tiempo y rango
+
+        //act
+        List pistas;
+        for(int i = 1; i < 4; i++){
+
+            pistas = policia.visitar(aeropuerto);
+            assertEquals(pistas.get(0).revelar(), pistaBuenosAires.revelar());
             assertEquals(policia.tiempoTranscurrido(), i);
         }
     }
