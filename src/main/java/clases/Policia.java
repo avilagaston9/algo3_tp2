f5f7ee20402d28a8d3fd2d4d32abd47d463b2a28
Gastón <gavila@fi.uba.ar>
2021-12-14 14:10:16 -0300

    Meto en paquetes y cambio edificio

--- src/main/java/clases/Policia.java
+++ src/main/java/clases/Policia.java
@@ -28,17 +28,11 @@ public class Policia {
         pistas.add(this.rango.pedirPistaLadron(ladron));
         return pistas;
     }
-
-<<<<<<< HEAD
-    public int tiempoTranscurridoEnHoras(){
-        return plazo.getHoras();
-=======
     public void dormir() { this.tiempoTranscurrido.dormir(); }
 
     public int tiempoTranscurridoEnHoras() {
 
         return this.tiempoTranscurrido.tiempoTranscurridoEnHoras();
->>>>>>> 3045364757e6e84e02fcf1f98fd474f965e4e92d
     }
 
     public void herirCon(Arma unArma) {  unArma.Herir(this.tiempoTranscurrido);  }
@@ -61,16 +55,10 @@ public class Policia {
 
         return unaCiudad == this.ciudadActual;
     }
-
-<<<<<<< HEAD
-    public boolean ciudadActual(Ciudad ciudad){
-        if (ciudadActual == ciudad){
+    public boolean ciudadActual(Ciudad ciudad) {
+        if (ciudadActual == ciudad) {
             return true;
         }
         return false;
-=======
-    public int tiempoTranscurrido() {
-        return this.tiempoTranscurrido.tiempoTranscurridoEnHoras();
->>>>>>> 3045364757e6e84e02fcf1f98fd474f965e4e92d
     }
 }
