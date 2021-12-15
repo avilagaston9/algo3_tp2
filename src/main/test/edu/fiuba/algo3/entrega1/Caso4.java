63bb61f70c431997a2a3b39fdef197806c8d038b
Gastón <gavila@fi.uba.ar>
2021-12-15 17:39:37 -0300

    Dejo listo caso 4 (entrega 1)

--- src/main/test/edu/fiuba/algo3/entrega1/Caso4.java
+++ src/main/test/edu/fiuba/algo3/entrega1/Caso4.java
@@ -1,9 +1,17 @@
 package edu.fiuba.algo3.entrega1;
 
 import clases.*;
+import clases.caracteristicasLadron.*;
+import clases.edificios.Aeropuerto;
+import clases.edificios.Edificio;
+import clases.ladron.Ladron;
+import clases.ladron.LadronBuilder;
+import clases.pistas.Pista;
+import clases.pistas.PistaFacil;
+import clases.rangos.*;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,77 +19,78 @@ public class Caso4 {
 
     @Test
     public void NovatoVisitaUnAeropuertoTresVeces(){
-        /*
 
         //arrange
-        Ciudad montreal = new Ciudad("Montreal");
-        Ciudad buenosAires = new Ciudad("Buenos Aires");
-        FabricaDePistas fabrica= new FabricaDePistas();
-        Rango rango=new Novato();
 
-        Edificio aeropuertoFrances = new Aeropuerto(fabrica);
-        Edificio ezeiza= new Aeropuerto(fabrica);
-        ezeiza.agregarPista("Su bandera es blanca y celeste",rango);
-        aeropuertoFrances.agregarPista("Su bandera es azul ,roja y blanca",rango);
-        montreal.agregarEdificio(aeropuertoFrances);
-        buenosAires.agregarEdificio(ezeiza);
+        //se crea al ladrón
+        LadronBuilder builder = new LadronBuilder();
+        builder.setCabello(new Rubio());
+        builder.setHobby(new Tenis());
+        builder.setSexo(new Femenino());
+        builder.setSenia(new Tatuaje());
+        builder.setVehiculo(new Moto());
+        Ladron maria = builder.getLadron();
 
-        Ladron maria = new Ladron("Maria");
-        buenosAires.agregarLadron(maria);
-        int distancia = 9000;
-        montreal.agregarSiguiente(buenosAires, distancia);
+        //se crea Montreal
+        Coordenadas coordenadasMontreal = new Coordenadas(45.50884,-73.58781);
+        Ciudad montreal = new Ciudad("Montreal", coordenadasMontreal);
+        //se crea policia novato en montreal
+        Policia policia = new Policia(new Novato(), montreal);
 
-        Policia policia = new Policia(rango, montreal); // crear tiempo
+        //se crea la pista
+        PistaFacil pistaPrueba = new PistaFacil("Su bandera es blanca y celeste");
 
-        //act
-        Pista pista;
-        Pista pistaEsperada= new PistaFacil();
-        pistaEsperada.agregarCaracteristica("Su bandera es blanca y celeste");
+        //se crea aeropuerto y se guarda la pista
+        Edificio aeropuerto = new Aeropuerto();
+        aeropuerto.setPista(pistaPrueba);
+
+        //policia visita aeropuerto 3 veces
+        LinkedList<Pista> pistasDevueltas;
         for(int i = 0; i < 3; i++){
 
-            pista = policia.visitar(aeropuertoFrances);
+            pistasDevueltas = policia.visitar(aeropuerto, maria);
             //Chequea que siempre sea la misma pista
-            assertEquals(pista.revelar(), pistaEsperada.revelar());
+            assertEquals(pistasDevueltas.get(0).revelar(), "Su bandera es blanca y celeste");
         }
-        assertEquals(policia.tiempoTranscurrido(), 0);
+        assertEquals(0, policia.tiempoTranscurridoEnHoras());
     }
 
     @Test
     public void NovatoVisitaUnAeropuerto55Veces(){
 
         //arrange
-        Ciudad montreal = new Ciudad("Montreal");
-        Ciudad buenosAires = new Ciudad("Buenos Aires");
-        FabricaDePistas fabrica= new FabricaDePistas();
-        Rango rango=new Novato();
 
+        //se crea al ladrón
+        LadronBuilder builder = new LadronBuilder();
+        builder.setCabello(new Rubio());
+        builder.setHobby(new Tenis());
+        builder.setSexo(new Femenino());
+        builder.setSenia(new Tatuaje());
+        builder.setVehiculo(new Moto());
+        Ladron maria = builder.getLadron();
 
-        Edificio aeropuertoFrances = new Aeropuerto(fabrica);
-        Edificio ezeiza= new Aeropuerto(fabrica);
-        aeropuertoFrances.agregarPista("Su bandera es azul,roja y blanca",rango);
-        ezeiza.agregarPista("Su bandera es blanca y celeste",rango);
-        montreal.agregarEdificio(aeropuertoFrances);
-        buenosAires.agregarEdificio(ezeiza);
+        //se crea Montreal
+        Coordenadas coordenadasMontreal = new Coordenadas(45.50884,-73.58781);
+        Ciudad montreal = new Ciudad("Montreal", coordenadasMontreal);
+        //se crea policia novato en montreal
+        Policia policia = new Policia(new Novato(), montreal);
 
-        Ladron maria = new Ladron("Maria");
-        buenosAires.agregarLadron(maria);
-        int distancia = 9000;
-        montreal.agregarSiguiente(buenosAires, distancia);
+        //se crea la pista
+        PistaFacil pistaPrueba = new PistaFacil("Su bandera es blanca y celeste");
 
-        Policia policia = new Policia(rango, montreal); // crear tiempo y rango
+        //se crea aeropuerto y se guarda la pista
+        Edificio aeropuerto = new Aeropuerto();
+        aeropuerto.setPista(pistaPrueba);
 
-        //act
-        Pista pista;
-        Pista pistaEsperada= new PistaFacil();
-        pistaEsperada.agregarCaracteristica("Su bandera es blanca y celeste");
+        //policia visita aeropuerto 3 veces
+        LinkedList<Pista> pistasDevueltas;
         for(int i = 0; i < 55; i++){
 
-            pista = policia.visitar(aeropuertoFrances);
+            pistasDevueltas = policia.visitar(aeropuerto, maria);
             //Chequea que siempre sea la misma pista
-            assertEquals(pista.revelar(),pistaEsperada.revelar());
+            assertEquals(pistasDevueltas.get(0).revelar(), "Su bandera es blanca y celeste");
         }
-        assertEquals(policia.tiempoTranscurrido(), 0);
+        assertEquals(0, policia.tiempoTranscurridoEnHoras());
 
-         */
     }
 }
