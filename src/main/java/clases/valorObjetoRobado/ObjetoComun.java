96698b7b2174c928eb859d620fa6b7604007b0cb
theo <tmiguel@fi.uba.ar>
2021-12-17 17:50:25 -0300

    agrego ObjetoRobado

---
+++ src/main/java/clases/valorObjetoRobado/ObjetoComun.java
@@ -0,0 +1,19 @@
+//package clases.valorObjetoRobado;
+//
+//import clases.Ciudad;
+//
+//import java.util.ArrayList;
+//
+//public class ObjetoComun implements ObjetoRobado{
+//
+//    private int largoRuta;
+//
+//    public ObjetoComun(){
+//        this.largoRuta = 4;
+//    }
+//
+//    @Override
+//    public ArrayList<Ciudad> crearRutaDeEscape(ArrayList<Ciudad> ciudades) {
+//
+//    }
+//}
