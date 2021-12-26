0ebf82ecd03d9b3fb08fc9b1e1ea35d01e2d187f
avilagaston9 <gavila@fi.uba.ar>
2021-12-26 18:24:47 -0300

    Intento implementar lector de json

--- src/main/java/clases/InteraccionConArchivos/LectorDeArchivos/LectorArchivoLadrones.java
+++ src/main/java/clases/InteraccionConArchivos/LectorDeArchivos/LectorArchivoLadrones.java
@@ -2,10 +2,18 @@ package clases.InteraccionConArchivos.LectorDeArchivos;
 
 
 import java.util.ArrayList;
+//import org.json.simple.parser.JSONParser;
 
 public class LectorArchivoLadrones implements LectorArchivos {
     @Override
     public ArrayList leer(String pathArchivo) {
+        /*
+
+        JSONParser parser = new JSONParser();
+
+
         return new ArrayList();
+         */
     }
+
 }
