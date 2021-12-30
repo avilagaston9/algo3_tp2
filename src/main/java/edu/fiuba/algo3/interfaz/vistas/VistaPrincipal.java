2f93887789952295d049c607379f60e28052056f
avilagaston9 <gavila@fi.uba.ar>
2021-12-30 10:56:19 -0300

    Voy refactorizando

--- src/main/java/edu/fiuba/algo3/interfaz/vistas/VistaPrincipal.java
+++ src/main/java/edu/fiuba/algo3/interfaz/vistas/VistaPrincipal.java
@@ -22,31 +22,6 @@ public class VistaPrincipal implements Observador {
 		algothief.setObservador(this);
 	}
 
-/*
-	public VistaPrincipal(Stage stage) throws FileNotFoundException {
-
-		this.stage = stage;
-
-		ContenedorBienvenidos contenedorBienvenidos = new ContenedorBienvenidos(stage, escenaPrincipal, algothief);
-		Scene escenaBienvenidos = new Scene(contenedorBienvenidos, 640, 480);
-
-		stage.setScene(escenaBienvenidos);
-		stage.setFullScreen(true);
-
-		stage.show();
-
-
-
-
-		this.algothief = algothief;
-		algothief.setObservador(this);
-
-		ContenedorPrincipal contenedorPrincipal = new ContenedorPrincipal(stage, algothief, ciudades, ladrones);
-		Scene escenaPrincipal = new Scene(contenedorPrincipal, 640, 480);
-
-	}
-
- */
 	@Override
 	public void actualizar() {
 
